97e4193d5f6ad83013270aada2624036aa686c27
rauljmza <rauljmendoza5@gmail.com>
2024-08-23 02:02:47 -0600

    Segundo Avance, Segundo Avance

--- toneladas_GEI_evitadas.py
+++ toneladas_GEI_evitadas.py
@@ -15,7 +15,31 @@ basuraGeneradaEnAcapulcoTON = 317173
 
 emisionesGEIGeneradasEnAcapulco = 412367
 
-emisionesGEIPorToneladaDeBausra = basuraGeneradaEnAcapulcoTON/emisionesGEIGeneradasEnAcapulco
+emisionesGEIPorToneladaDeBausra = emisionesGEIGeneradasEnAcapulco/basuraGeneradaEnAcapulcoTON
 
 
+puntosUsuario = int(input("Ingresa el número de puntos que tienes en tu BioWay App: "))
 
+numVecesUsuarioBrinda = puntosUsuario/20 #Cada vez que el usuario brinda sus reciclables obtiene 20 puntos
+
+basuraTotalUsuario = basuraGeneradaPorPersonaPorDia * numVecesUsuarioBrinda #Determinar la basura reciclable brindada
+
+"Cuánto representa cada material (cartón, papel, aluminio) respecto a la basura total para determinar la basura brindada"
+plasticoReciclado = basuraTotalUsuario * porcentajePlastico #en kg
+
+cartonPapelReciclado = basuraTotalUsuario * porcentajeCartonPapel #en kg
+
+"Calculo de emisiones CO2-eq evitadas"
+totalReciclado = plasticoReciclado + cartonPapelReciclado
+
+"                    Convertir kg a toneladas"
+emisionesEvitadas = (totalReciclado / 1000) * emisionesGEIPorToneladaDeBausra
+
+#El ":.2f" es para reducirlo a 2 decimales los resultados
+
+print(f"Felicidades! Has brindado un total de {numVecesUsuarioBrinda} veces tus reciclables, aproximadamente {basuraTotalUsuario} kg de residuos")
+print(f"De los cuales {plasticoReciclado} kg son plastico, y {cartonPapelReciclado:.2f} kg son de Cartón y/o Papel.")
+print(f"¡Has evitado aproximadamente {emisionesEvitadas:.2f} toneladas de CO2 por reciclar!")
+print("Recuerda que con BioWay Juntos por un Mundo Mejor")
+
+"Esto equivaldría a....." #HACER
