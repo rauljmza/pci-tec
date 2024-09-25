7457216810daf8dcdddf569c8805cdbdc8afd6eb
rauljmza <rauljmendoza5@gmail.com>
2024-09-24 22:08:01 -0600

    Ciclos While

--- toneladas_GEI_evitadas.py
+++ toneladas_GEI_evitadas.py
@@ -3,10 +3,10 @@ Proyecto Emisiones de Gases de Efecto Invernadero (ton) evitadas con BioWay
 Calcula las emisiones GEI evitadas por reciclar en BioWay.
 """
 
-basuraGeneradaPorPersonaPorDia = 1.1 # kg de basura generada por persona al día
-porcentajePlastico = 0.11 # 11% de la basura total son plasticos
-porcentajeCartonPapel = 0.142 # 14.2% corresponde a papel y cartón
-porcentajeAluminio = 0.018 # 1.8% corresponde al aluminio
+BASURA_GENERADA_POR_PERSONA_AL_DIA = 1.1 # kg de basura generada por persona al día
+PORCENTAJE_PLASTICO = 0.11 # 11% de la basura total son plasticos
+PORCENTAJE_CARTON_PAPEL = 0.142 # 14.2% corresponde a papel y cartón
+PORCENTAJE_ALUMINIO = 0.018 # 1.8% corresponde al aluminio
 
 """Emisiones GEI (CO2-eq) Disposición de RSU (toneladas) / Reciclados RSU (toneladas)"
 De acuerdo a datos de Acapulco obtenidos de Emisiones de Gases de Efecto Invernadero en Vertederos de Residuos Sólidos Urbanos (2017), 317,173
@@ -21,47 +21,85 @@ emisionesGEIPorToneladaDeBausra = emisionesGEIGeneradasEnAcapulco/basuraGenerada
 
 """Los usuarios en BioWay App, cada vez que birndan sus reciclables obtienen 20 puntos"""
 
-puntosUsuario = int(input("Ingresa el número de puntos que tienes en tu BioWay App: "))
+#Se añade una función que establece una meta de emisiones evitadas, dicha meta establecida por el usuario
 
-numVecesUsuarioBrinda = puntosUsuario/20 #Cada vez que el usuario brinda sus reciclables obtiene 20 puntos
+def materialReciclado(basura,porcentajeMaterial):
+            return basura * porcentajeMaterial
 
-basuraTotalUsuario = basuraGeneradaPorPersonaPorDia * numVecesUsuarioBrinda #Determinar la basura reciclable brindada
-#__________________________________________________________________________________________________________________________
+def metaAlcanzarEmisiones():
+    
+    while True:
+            metaEmisiones = float(input("Ingresa la meta de toneladas de emisiones GEI que deseas evitar: "))
+            if metaEmisiones <=0:
+                print("La meta debe ser de un número positivo.")
+                continue
+            break
+    
+    puntosUsuario = int(input("Ingresa el número de puntos que tienes en tu BioWay App: "))
 
-"¿Cuánto representa cada material (cartón, papel, aluminio) respecto a la basura total para determinar la basura brindada?"
+    #numVecesUsuarioBrinda = puntosUsuario/20 #Cada vez que el usuario brinda sus reciclables obtiene 20 puntos
+    numVecesUsuarioBrinda = 0
+    
+    while puntosUsuario >= 20:
+        puntosUsuario -= 20
+        numVecesUsuarioBrinda += 1
 
-plasticoReciclado = basuraTotalUsuario * porcentajePlastico #en kg
+    basuraTotalUsuario = BASURA_GENERADA_POR_PERSONA_AL_DIA * numVecesUsuarioBrinda #Determinar la basura reciclable brindada
+    #__________________________________________________________________________________________________________________________
 
-cartonPapelReciclado = basuraTotalUsuario * porcentajeCartonPapel #en kg
+    "¿Cuánto representa cada material (cartón, papel, aluminio) respecto a la basura total para determinar la basura brindada?"
 
-aluminioReciclado = basuraTotalUsuario * porcentajeAluminio #en kg
+    #PUEDE CONVERTIRSE EN UNA FUCNION FUNCION (basuraTotalUsuario, Porcentaje_Plastico)
 
-#__________________________________________________________________________________________________________________________
+        
+    
+    plasticoReciclado = materialReciclado(basuraTotalUsuario,PORCENTAJE_PLASTICO) #en kg
 
-"Calculo de emisiones CO2-eq evitadas"
-totalReciclado = plasticoReciclado + cartonPapelReciclado
+    cartonPapelReciclado = materialReciclado(basuraTotalUsuario,PORCENTAJE_CARTON_PAPEL) #en kg
 
-"                    Convertir kg a toneladas"
-emisionesEvitadas = (totalReciclado / 1000) * emisionesGEIPorToneladaDeBausra
+    aluminioReciclado = materialReciclado(basuraTotalUsuario,PORCENTAJE_ALUMINIO) #en kg
 
-toenladas_GEI_por_kilometro_carro = 143 / 1000000
-toneladas_GEI_año_carro = toenladas_GEI_por_kilometro_carro * 15000 #15,000 km
+    #__________________________________________________________________________________________________________________________
 
-#_________________________________________________________________________________________________________________________
+    "Calculo de emisiones CO2-eq evitadas"
+    totalReciclado = plasticoReciclado + cartonPapelReciclado + aluminioReciclado
+
+    "                    Convertir kg a toneladas"
+    emisionesEvitadas = (totalReciclado / 1000) * emisionesGEIPorToneladaDeBausra
+
+    #_________________________________________________________________________________________________________________________
+    toenladas_GEI_por_kilometro_carro = 143 / 1000000
+    toneladas_GEI_año_carro = toenladas_GEI_por_kilometro_carro * 15000 #15,000 km
+
+    #_________________________________________________________________________________________________________________________
 
 
-#El ":.2f" es para reducirlo a 2 decimales los resultados
+    #El ":.2f" es para reducirlo a 2 decimales los resultados
 
-print(f"Felicidades! Has brindado un total de {numVecesUsuarioBrinda} veces tus reciclables, aproximadamente {basuraTotalUsuario} kg de residuos")
-print(f"De los cuales {plasticoReciclado:.2f} kg son plastico, y {cartonPapelReciclado:.2f} kg son de Cartón y/o Papel.")
-print(f"¡Has evitado aproximadamente {emisionesEvitadas:.2f} toneladas de CO2 por reciclar!")
+    print(f"\nFelicidades! Has brindado un total de {numVecesUsuarioBrinda} veces tus reciclables, aproximadamente {basuraTotalUsuario:.2f} kg de residuos.")
+    print(f"\nDe los cuales {plasticoReciclado:.2f} kg son plastico, y {cartonPapelReciclado:.2f} kg son de Cartón y/o Papel.")
+    print(f"\n¡Has evitado aproximadamente {emisionesEvitadas:.2f} toneladas de CO2 por reciclar!")
+    
+    #_________________________________________________________________________________________________________________________
+    
+    #Verificar si alcanzó la meta
+    if emisionesEvitadas > metaEmisiones:
+        print(f"¡Felicidades! Has alcanzado tu meta de evitar {metaEmisiones:.2f} toneladas de CO2.")
+    else:
+        #Calcular cuántas veces más debe brindar para alcanzar la meta por regla de tres
+        vecesFaltantes = (metaEmisiones * numVecesUsuarioBrinda)/emisionesEvitadas
+        print(f"\nTe faltan aproximadamente {vecesFaltantes:.2f} veces más de brindar basura para alcanzar tu meta.")
 
-#Equivalencia de toneladas de CO2 evitadas
 
-"""Un promedio normal que se puede esperar del recorrido de un vehiculo en un año oscila entre 15000 a 27000"""
-if emisionesEvitadas >= toneladas_GEI_año_carro:
-    print(f"Has evitado el equivalente al uso del carro durante un año")
 
-print("Recuerda que con BioWay Juntos por un Mundo Mejor")
+    #Equivalencia de toneladas de CO2 evitadas PUEDE SER UNA FUNCION TAMBIEN funcion(emisionesEvitadas)
 
-"Esto equivaldría a....." #HACER
+    """Un promedio normal que se puede esperar del recorrido de un vehiculo en un año oscila entre 15000 a 27000"""
+    if emisionesEvitadas >= toneladas_GEI_año_carro:
+        print(f"Has evitado el equivalente al uso del carro durante un año")
+
+    print("\nRecuerda que con BioWay Juntos por un Mundo Mejor")
+
+    "Esto equivaldría a....." #HACER    
+
+metaAlcanzarEmisiones()
